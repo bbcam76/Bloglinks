d031adc09cb955bf6b33df2cb619465557e4f823
funtari76 <funtari777@gmail.com>
2022-09-10 09:02:44 +0000

    started changes in views

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,12 @@
 """Todo app models"""
 from django.shortcuts import render
+from .models import Item
 
 
 def get_todo_list(request):
     """ Function to render the todo list """
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
