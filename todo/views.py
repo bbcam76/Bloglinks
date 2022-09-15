2da9f44eb22a0693a1da4129f9bf342b2499cc28
funtari76 <funtari777@gmail.com>
2022-09-15 21:02:56 +0000

    changed the viewsa and added # noqa with not much changes

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,21 @@
 """Todo app models"""
 from django.shortcuts import render
+from django.views import generic
 from .models import Bloglinks
 
 
 def get_todo_list(request):
     """ Function to render the todo list """
-    items = Bloglinks.objects.all()
+    items = Bloglinks.objects.all()  # noqa
     context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+class PostList(generic.ListView):
+    """ Post list views """
+    model = Bloglinks
+    queryset = Bloglinks.objects.filter(status=1).order_by('-created_on')  # noqa
+    template_name = 'index.html'
+    paginate_by = 6
