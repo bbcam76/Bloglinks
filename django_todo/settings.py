b95721975b93e332ba042db6ec17bf760a523b19
funtari76 <funtari777@gmail.com>
2022-09-18 09:28:19 +0000

    create runtime file, update settings for cloudinary

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+
+    # Static storages
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
