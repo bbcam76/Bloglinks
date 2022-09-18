1a78b6759acbc79212132be78492dd0b08f2772e
funtari76 <funtari777@gmail.com>
2022-09-18 09:11:28 +0000

    update models and create requiremnets.txt

--- todo/models.py
+++ todo/models.py
@@ -18,7 +18,6 @@ class Bloglinks(models.Model):
     excerpt = models.TextField(blank=True, default=None)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True, default=None)
 
     class Meta:
         ordering = ['-created_on']
