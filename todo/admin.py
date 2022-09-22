46281f0251c36a07f5e44e02ed58aa5c8c019d79
funtari76 <funtari777@gmail.com>
2022-09-22 13:55:52 +0000

    had erorrsin the Admin fixed and changed the views

--- todo/admin.py
+++ todo/admin.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Bloglinks, Comment
 
-class BloglinksAdmin(SummernoteModelAdmin):
-   """ Summernote admin class """
 
-   summernote_fields = ('content')
+class BloglinksAdmin(SummernoteModelAdmin):
+    """ Summernote admin class """
+
+    summernote_fields = ('content')
+
 
 admin.site.register(Bloglinks)
 admin.site.register(Comment)
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,5 @@
 """Todo app models"""
 from django.shortcuts import render
-from django.views import generic
 from .models import Bloglinks
 
 
@@ -11,11 +10,3 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
-
-
-#class PostList(generic.ListView):
-#    """ Post list views """
-#    model = Bloglinks
-#    queryset = Bloglinks.objects.filter(status=1).order_by('-created_on')  # noqa
-#    template_name = 'index.html'
-#    paginate_by = 6
