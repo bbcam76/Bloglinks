66a2e38a7a76dafc863788cab221cd302bcb8dd1
funtari76 <funtari777@gmail.com>
2023-04-13 18:36:27 +0000

    django crispy forms

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'home',
     'todo',
 
@@ -67,6 +68,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRIPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ todo/forms.py
@@ -0,0 +1,7 @@
+from .models import Comment
+from django import crispy_forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        field = ('body',)
