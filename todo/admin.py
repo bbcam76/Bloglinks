e23114f7ac5242c38c4774368b9ee4378f82d9e9
funtari76 <funtari777@gmail.com>
2023-06-02 15:39:55 +0000

    extened the admin

--- todo/admin.py
+++ todo/admin.py
@@ -8,8 +8,21 @@ from .models import Bloglinks, Comment
 class BloglinksAdmin(SummernoteModelAdmin):
     """ Summernote admin class """
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 # admin.site.register(Bloglinks)
-admin.site.register(Comment)
+# admin.site.register(Comment)
